c53b26f3cdeafaa29b877b682fb2bc4e624b181c
Mangesh-Shirodkar <68989269+Mangesh-Shirodkar@users.noreply.github.com>
2021-03-17 13:19:22 +0530

    added files from local side
    Added Farmer Registration report

---
+++ templates/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+from django.core import serializers
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("registration/", views.registration, name="registration"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="user_logout"),
+    path("Form2/", views.Form2, name="Form2"),
+    path("Form3/", views.Form3, name="Form3"),
+    path("Form4/", views.Form4, name="Form4"),
+    path("get_form1/<int:id>", views.get_form1, name="get_form1"),
+    path("get_form2/<int:id>", views.get_form2, name="get_form2"),
+    path("get_form4/<int:id>", views.get_form4, name="get_form4"),
+    path("Report1/", views.Report1, name="Report1")
+
+]
